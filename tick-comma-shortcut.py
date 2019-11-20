ce9051acd039c1410bd6764d3c06f38ca508ccb7
Dana Salk <46821074+danajsalk@users.noreply.github.com>
2019-11-20 09:33:03 -0700

    docs: updated

--- tick-comma-shortcut.py
+++ tick-comma-shortcut.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Mar 28 13:40:19 2018
-
-@author: dkoczur
-198504XX5, 254845GC8, 442403NQ0, 557363DH8, 5816637K1, 64990ED53, 677522ZB3, 686516KH7, 707332PT3, 709235TD7, 735240J38, 849476RU2, 87971HFB5, 93974DRR1
-
+Simple shortcut to add ticks to a comma seperated string
 
 """
 
